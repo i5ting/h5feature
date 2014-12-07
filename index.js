576adf8b2080aecccce46f81cf907bbe69265201
alfred sang <i5ting@126.com>
2014-12-07 22:57:42 +0800

    init

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-var request = require('request');
-request('http://html5test.com/compare/feature/index.html', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
+// var request = require('request');
+// request('http://html5test.com/compare/feature/index.html', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Print the google web page.
+//   }
+// })
+
+
+// Print all of the news items on Hacker News
+var jsdom = require("jsdom");
+
+jsdom.env({
+  url: "http://html5test.com/compare/feature/index.html",
+  scripts: ["http://code.jquery.com/jquery.js"],
+  done: function (errors, window) {
+    var $ = window.$;
+    console.log("HN Links");
+    $("table").each(function() {
+      console.log(" -", $(this));
+    });
   }
-})
+});
--- readme.md
+++ readme.md
@@ -1,2 +1,39 @@
-https://github.com/request/request
+# mobile h5 storage
 
+http://html5test.com/compare/feature/storage-localStorage/storage-sqlDatabase/storage-indexedDB.basic.html
+
+一个残酷的现实。。。。。
+
+## ios
+
+![](img/ios.png)
+
+## android
+
+![](img/a.png)
+
+## chrome
+
+![](img/c.png)
+
+# 设计
+
+## deteck模块
+
+检测顺序
+
+1. webSQL
+1. indexedDB
+1. localStorage
+1. memory + sifter.js
+
+## api base类 
+
+
+## cruds
+
+- 增加
+- 删除
+- 修改
+- 查询
+- 搜索
